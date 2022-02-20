ddb1067194f536bf6678cbb59ba7f629f78d35f4
Bort Sparkle <bortsparkle@gmail.com>
2022-02-20 13:05:32 -0800

    added comments to plugin file so it shows up in wordpress

---
+++ posteebreak.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* Plugin Name: posteebreak
+* Plugin URI: https://github.com/bortsparkle/posteebreak
+* Description: Breaks plaintext posts from Postie into paragraph blocks at blank lines
+* Version: 0.0.2022.02.20.1303
+* Author: Someone Else
+* Author URI: https://github.com/bortsparkle
+**/
+?>
