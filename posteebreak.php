1c80733ebf9fbcaabbd8a36d3b4c352467658b87
Bort Sparkle <bortsparkle@gmail.com>
2022-02-27 10:36:34 -0800

    added postie hook

--- posteebreak.php
+++ posteebreak.php
@@ -3,8 +3,16 @@
 * Plugin Name: posteebreak
 * Plugin URI: https://github.com/bortsparkle/posteebreak
 * Description: Breaks plaintext posts from Postie into paragraph blocks at blank lines
-* Version: 0.0.2022.02.20.1303
+* Version: 0.0.2022.02.27.1033
 * Author: Someone Else
 * Author URI: https://github.com/bortsparkle
 **/
+
+function posteebreak_filter_postie_post($post) {
+    //do something here like update $post['post_content']
+    return $post;
+}
+
+add_filter('postie_post_before', 'posteebreak_filter_postie_post');
+
 ?>
