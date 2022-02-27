9c29caf50796135a9a021acad0eb87ab31eb919c
Bort Sparkle <bortsparkle@gmail.com>
2022-02-27 11:24:14 -0800

    syntax error

--- posteebreak.php
+++ posteebreak.php
@@ -3,14 +3,13 @@
 * Plugin Name: posteebreak
 * Plugin URI: https://github.com/bortsparkle/posteebreak
 * Description: Breaks plaintext posts from Postie into paragraph blocks at blank lines
-* Version: 0.0.2022.02.27.1117
+* Version: 0.0.2022.02.27.1124
 * Author: Someone Else
 * Author URI: https://github.com/bortsparkle
 **/
 
 function posteebreak_filter_postie_post($post) {
     //do something here like update $post['post_content']
-    $filtered= $post['post_content']
 
     $filtered = "filtered by posteebreak\n";
     $filtered .= $filtered;
