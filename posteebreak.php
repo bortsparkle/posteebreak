e917180a25894f8c49889b2c3177ff74fd5e2000
Bort Sparkle <bortsparkle@gmail.com>
2022-02-27 10:56:34 -0800

    trying again

--- posteebreak.php
+++ posteebreak.php
@@ -3,15 +3,17 @@
 * Plugin Name: posteebreak
 * Plugin URI: https://github.com/bortsparkle/posteebreak
 * Description: Breaks plaintext posts from Postie into paragraph blocks at blank lines
-* Version: 0.0.2022.02.27.1047
+* Version: 0.0.2022.02.27.1056
 * Author: Someone Else
 * Author URI: https://github.com/bortsparkle
 **/
 
 function posteebreak_filter_postie_post($post) {
     //do something here like update $post['post_content']
-    $filtered = $post;
-    $filtered = "posteebreak filtered this\n{$filtered}\nand now it's done";
+    $filtered = "filtered by posteebreak\n";
+    $filtered .= $post;
+    $filtered .= "\nand now it is done filtering";
+
     return $filtered;
 }
 
